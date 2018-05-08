a7fa25316e5abb37fcab44995c809fe5012b5b45
tabbatharenea@gmail.com <Renea415!>
2018-05-08 06:23:15 -0400

    added if conditional to showForm() to check that clicked table is available, refactored saveForm()

--- script.js
+++ script.js
@@ -2,50 +2,52 @@
 
 $(document).ready(() => {
 
-let tableNumber;
 
 const reservations = [];
 
+let tableNumber;
 function showForm() {
+  //if statement to check if the clicked table is available
+ if($(this).hasClass("available")){
+   //fade the form in and set to flex
   $("#resForm").fadeIn(500)
   .css("display", "flex")
+  //set the table number
   tableNumber = this.id;
+  //change the table number text on the pop up form
   $("#tableNum").text(tableNumber);
+  //console.log for testing
   console.log(`Table number to be reserved is ${tableNumber}`);
-  
+ }
 };
 
 function hideForm() {
   $("#resForm").fadeOut(500)
-  // .css("display", "none")
 }
 
 
 function saveForm(){
 
 
-  let newReservation = {
-    name : $("#resName").val(),
+let reservation = {
+    name: $("#resName").val(),
     phone: $("#resPhone").val(),
     partySize: $("#resPartySize").val()
-  }
+}
 
-  reservations.push(newReservation); //this isn't working yet
+  // reservations.push(newReservation); //this isn't working yet
 
-  $(`#${tableNumber}`).toggleClass("reserved");
-
-  // console.log(`${tableNumber}`);
-  // console.log(`You're booking table number ${tableNumber}`);
-  // hideForm();
+  $(`#${tableNumber}`).addClass("reserved").removeClass("available");
+  
   console.log(`
   Your new reservation details are:
-  Name: ${newReservation.name}
-  Phone: ${newReservation.phone}
-  Party size: ${newReservation.partySize}
+  Name: ${reservation.name}
+  Phone: ${reservation.phone}
+  Party size: ${reservation.partySize}
   `);
-  
+
   hideForm();
-  showConfirmation();
+  // showConfirmation(); this is not working yet
 }
 
 function showConfirmation(newReservation){
