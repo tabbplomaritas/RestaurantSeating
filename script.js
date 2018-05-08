2d7a7435e20ddc8425e70a497babaa4ad9232441
tabbatharenea@gmail.com <Renea415!>
2018-05-08 19:40:25 -0400

    testing for mouseover .reserved tables to work on tables that are changed to .reserved when subtmi() is run

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 "use strict";
 
-$(document).ready(() => {
+{
 
 class ResTracker {
   constructor(reservations){
     this.reservations = [
-      new NewRes("Tabbatha", "5555555555", 2, 1)
+      new NewRes("Tabbatha", "5555555555", 2, "2"),
+      new NewRes("Lindsey", "22222222222", 5, "1")
     ]
   }
 }
@@ -19,13 +20,14 @@ class NewRes {
     this.bookedTable = bookedTable;
   }
 }
-
+//create instance of resTracker
 const myResTracker = new ResTracker();
 console.log(myResTracker);
 
 
+
 let tableNumber;
-function showForm(event) {
+function showForm() {
   //if statement to check if the clicked table is available
  if($(this).hasClass("available")){
    //fade the form in and set to flex
@@ -57,7 +59,7 @@ function saveForm(){
   (info.name, info.phone, info.partySize, info.tableNumber);
 
   myResTracker.reservations.push(addRes);
-  console.log(`***${myResTracker.reservations[0]}`)
+  console.log(`***${myResTracker.reservations}`)
 
 
 
@@ -72,22 +74,54 @@ function saveForm(){
   `);
 
   hideForm();
+  resetFormValues();
 
-  //reset input values
-  // $("#resName").val() = "";
-  // $("#resPhone").val() = "";
-  // $("#resPartySize").val() = "";
-  // showConfirmation(); this is not working yet
 }
 
-function showConfirmation(newReservation){
-  $("#confirm").css("display", "flex")
-  $("#confirmNum").text(`${tableNumber}`);
-  $("#confirmName").text(`Name: ${newReservation.name}`);
+function resetFormValues(){
+  $("input[type=text], textarea").val("");
 }
 
+//this function isn't needed for projeect, but I'd like to come back to it if I have time
+// function showConfirmation(newReservation){
+//   $("#confirm").css("display", "flex");
+//   $("#confirmNum").text(`${tableNumber}`);
+//   $("#confirmName").text(`Name: ${newReservation.name}`);
+// }
 
 
+
+$(".reserved").on("mouseover", function(){
+ //set the id of the clicked element to a variable
+  let clickedItem = this.id;
+
+  //check taht number against all the booked tables in our reservatiosn array
+  for(let i = 0; i < myResTracker.reservations.length; i++){
+    if(myResTracker.reservations[i].bookedTable == `${clickedItem}`){
+     //when we find a match, grab the popup <p> inside the clicked item, set it to a variable, fade it in, and populate it with the data from the found reservation object
+      let popUpDiv = ($(this).find('.popUp'));
+      popUpDiv.slideDown(500);
+
+      console.log("this is working.");//test to see if the mouseover is working at all on the .reserved circles(it's not)
+
+      popUpDiv.html(`
+      <p>Name: ${myResTracker.reservations[i].name}</p>
+      <p>Phone: ${myResTracker.reservations[i].phone}</p>
+      <p>Party Size: ${myResTracker.reservations[i].partySize}</p>
+      <p>Table Booked: ${myResTracker.reservations[i].bookedTable}</p>
+      `);
+      }
+  }
+})
+
+  $(".reserved").on("mouseout", function(){
+    let popUpDiv = ($(this).find('.popUp'));
+    popUpDiv.slideUp(500);
+
+  });
+
+
+  
 $(".available").click(showForm);
 $("#closeFormButton").click(hideForm);
 $("#saveButton").click(saveForm);  
@@ -102,7 +136,7 @@ $("#confirmCloseButton").click(function(){
 
 
 
-});
+}
   
 
 
