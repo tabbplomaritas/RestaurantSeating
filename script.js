16056b121d5e3e1443520e5cddc819938b0a15ff
tabbatharenea@gmail.com <Renea415!>
2018-05-09 09:04:03 -0400

    fixed hover blip iissue

--- script.js
+++ script.js
@@ -5,8 +5,8 @@
 class ResTracker {
   constructor(reservations){
     this.reservations = [
-      new NewRes("Tabbatha", "5555555555", 2, "2"),
-      new NewRes("Lindsey", "22222222222", 5, "1")
+      new NewRes("Tabbatha", "5555555555", 2, 2),
+      new NewRes("Lindsey", "22222222222", 5, 1)
     ]
   }
 }
@@ -56,10 +56,10 @@ function saveForm(){
   }
 
   const addRes = new NewRes
-  (info.name, info.phone, info.partySize, info.tableNumber);
+  (info.name, info.phone, info.partySize, info.bookedTable);
 
   myResTracker.reservations.push(addRes);
-  console.log(`***${myResTracker.reservations}`)
+  console.log(`***${myResTracker.reservations[0]}`)
 
 
 
@@ -82,20 +82,11 @@ function resetFormValues(){
   $("input[type=text], textarea").val("");
 }
 
-//this function isn't needed for projeect, but I'd like to come back to it if I have time
-// function showConfirmation(newReservation){
-//   $("#confirm").css("display", "flex");
-//   $("#confirmNum").text(`${tableNumber}`);
-//   $("#confirmName").text(`Name: ${newReservation.name}`);
-// }
 
-
-
-$(".reserved").on("mouseover", function(){
- //set the id of the clicked element to a variable
+$("#floorPlan").on('mouseenter', '.reserved', function(){ 
   let clickedItem = this.id;
-
-  //check taht number against all the booked tables in our reservatiosn array
+console.log("i'm mousing over a reserved element");
+  //check that number against all the booked tables in our reservation array
   for(let i = 0; i < myResTracker.reservations.length; i++){
     if(myResTracker.reservations[i].bookedTable == `${clickedItem}`){
      //when we find a match, grab the popup <p> inside the clicked item, set it to a variable, fade it in, and populate it with the data from the found reservation object
@@ -112,9 +103,62 @@ $(".reserved").on("mouseover", function(){
       `);
       }
   }
-})
 
-  $(".reserved").on("mouseout", function(){
+  });
+
+// $(document).on('mouseover', (function(e){
+  
+//   if (checkItem.hasClass("reserved")) {
+//     let clickedItem = this.id;
+
+//     //check that number against all the booked tables in our reservation array
+//     for(let i = 0; i < myResTracker.reservations.length; i++){
+//       if(myResTracker.reservations[i].bookedTable == `${clickedItem}`){
+//        //when we find a match, grab the popup <p> inside the clicked item, set it to a variable, fade it in, and populate it with the data from the found reservation object
+//         let popUpDiv = ($(this).find('.popUp'));
+//         popUpDiv.slideDown(500);
+  
+//         console.log("this is working.");//test to see if the mouseover is working at all on the .reserved circles(it's not)
+  
+//         popUpDiv.html(`
+//         <p>Name: ${myResTracker.reservations[i].name}</p>
+//         <p>Phone: ${myResTracker.reservations[i].phone}</p>
+//         <p>Party Size: ${myResTracker.reservations[i].partySize}</p>
+//         <p>Table Booked: ${myResTracker.reservations[i].bookedTable}</p>
+//         `);
+//         }
+//     }
+    
+//   }
+// });
+
+
+
+
+// $(".reserved").on("click", function(){
+//  //set the id of the clicked element to a variable
+//   let clickedItem = this.id;
+
+//   //check that number against all the booked tables in our reservation array
+//   for(let i = 0; i < myResTracker.reservations.length; i++){
+//     if(myResTracker.reservations[i].bookedTable == `${clickedItem}`){
+//      //when we find a match, grab the popup <p> inside the clicked item, set it to a variable, fade it in, and populate it with the data from the found reservation object
+//       let popUpDiv = ($(this).find('.popUp'));
+//       popUpDiv.slideDown(500);
+
+//       console.log("this is working.");//test to see if the mouseover is working at all on the .reserved circles(it's not)
+
+//       popUpDiv.html(`
+//       <p>Name: ${myResTracker.reservations[i].name}</p>
+//       <p>Phone: ${myResTracker.reservations[i].phone}</p>
+//       <p>Party Size: ${myResTracker.reservations[i].partySize}</p>
+//       <p>Table Booked: ${myResTracker.reservations[i].bookedTable}</p>
+//       `);
+//       }
+//   }
+// })
+
+$("#floorPlan").on('mouseleave', '.reserved', function(){
     let popUpDiv = ($(this).find('.popUp'));
     popUpDiv.slideUp(500);
 
@@ -128,12 +172,6 @@ $("#saveButton").click(saveForm);
 $("#confirmCloseButton").click(function(){
   $("#confirm").fadeOut(1000);
 });
-
-
-    
-    
-
-
 
 
 }
